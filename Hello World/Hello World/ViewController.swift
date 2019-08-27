9d5c79dbd44af2d2a65c9219c8b09565c2ad0186
Gavin Munoz <gavin.e.munoz@gmail.com>
2019-08-27 12:22:22 -0500

    Hello World App

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by TTA Student on 8/6/19.
+//  Copyright © 2019 TTA Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
